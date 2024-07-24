7ccf381d5e207ce61dbc32356b49982f1e910f93
Jonas Karlsson <96485472+kalsson@users.noreply.github.com>
2024-07-24 09:08:52 +0200

    Fixed the folder structure for all projects.

---
+++ Console/Sports Results/SportsResultsApp.kalsson/Services/DataScraper.cs
@@ -0,0 +1,50 @@
+﻿using HtmlAgilityPack;
+using SportsResultsApp.kalsson.Models;
+
+namespace SportsResultsApp.kalsson.Services;
+
+public class DataScraper
+{
+    private const string Url = "https://www.basketball-reference.com/boxscores/";
+
+    public async Task<List<BasketballGameModel>> ScrapeBasketballGamesAsync()
+    {
+        var httpClient = new HttpClient();
+        var html = await httpClient.GetStringAsync(Url);
+
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(html);
+
+        var games = new List<BasketballGameModel>();
+
+        var gameNodes = htmlDocument.DocumentNode.SelectNodes("//div[contains(@class, 'game_summary')]");
+        if (gameNodes != null)
+        {
+            foreach (var gameNode in gameNodes)
+            {
+                var winnerNode = gameNode.SelectSingleNode(".//table/tbody/tr[1]");
+                var loserNode = gameNode.SelectSingleNode(".//table/tbody/tr[2]");
+
+                if (winnerNode != null && loserNode != null)
+                {
+                    var team1 = winnerNode.SelectSingleNode("td[1]/a")?.InnerText.Trim();
+                    var team1Score = int.Parse(winnerNode.SelectSingleNode("td[2]")?.InnerText.Trim() ?? "0");
+                    var team2 = loserNode.SelectSingleNode("td[1]/a")?.InnerText.Trim();
+                    var team2Score = int.Parse(loserNode.SelectSingleNode("td[2]")?.InnerText.Trim() ?? "0");
+
+                    var game = new BasketballGameModel
+                    {
+                        Team1 = team1,
+                        Team1Score = team1Score,
+                        Team2 = team2,
+                        Team2Score = team2Score
+                    };
+
+                    games.Add(game);
+                }
+            }
+        }
+
+        return games;
+    }
+}
---
+++ Console/Sports Results/SportsResultsApp.kalsson/Services/EmailSender.cs
@@ -0,0 +1,52 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using SportsResultsApp.kalsson.Models;
+using System.Text;
+using SportsResultsApp.kalsson.Utils;
+
+namespace SportsResultsApp.kalsson.Services;
+
+public class EmailSender
+{
+    private readonly string _smtpServer;
+    private readonly int _smtpPort;
+    private readonly string _senderEmail;
+    private readonly string _senderPassword;
+    private readonly string _recipientEmail;
+
+    public EmailSender(string smtpServer, int smtpPort, string senderEmail, string senderPassword,
+        string recipientEmail)
+    {
+        _smtpServer = smtpServer ?? throw new ArgumentNullException(nameof(smtpServer));
+        _smtpPort = smtpPort;
+        _senderEmail = senderEmail ?? throw new ArgumentNullException(nameof(senderEmail));
+        _senderPassword = senderPassword ?? throw new ArgumentNullException(nameof(senderPassword));
+        _recipientEmail = recipientEmail ?? throw new ArgumentNullException(nameof(recipientEmail));
+    }
+
+    public async Task SendEmailAsync(List<BasketballGameModel> games)
+    {
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("Sports Results Notifier", _senderEmail));
+        message.To.Add(new MailboxAddress("Recipient", _recipientEmail));
+        message.Subject = "Daily Basketball Game Results";
+
+        var bodyBuilder = new BodyBuilder();
+        var sb = new StringBuilder();
+        sb.AppendLine("Here are the results of the basketball games played today:\n");
+
+        foreach (var game in games)
+        {
+            sb.AppendLine(game.ToString());
+        }
+
+        bodyBuilder.TextBody = sb.ToString();
+        message.Body = bodyBuilder.ToMessageBody();
+
+        using var client = new SmtpClient();
+        await client.ConnectAsync(_smtpServer, _smtpPort, MailKit.Security.SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync(_senderEmail, _senderPassword);
+        await client.SendAsync(message);
+        await client.DisconnectAsync(true);
+    }
+}
